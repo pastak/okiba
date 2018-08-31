2d6d7b52bbffc5ae9f2cb56a13f122a81174986f
pastak <pasta0915@gmail.com>
2018-08-31 15:44:27 +0900

    update md2sb v1.0.1

--- scrapbox/rollup.config.js
+++ scrapbox/rollup.config.js
@@ -3,9 +3,14 @@ import commonjs from 'rollup-plugin-commonjs'
 import builtins from 'rollup-plugin-node-builtins'
 import globals from 'rollup-plugin-node-globals'
 import json from 'rollup-plugin-json'
+import babel from 'rollup-plugin-babel'
 
 export default {
   plugins: [
+    babel({
+      babelrc: false,
+      presets: [['@babel/preset-env', { modules: false }]]
+    }),
     resolve({
       browser: true,
       extensions: [ '.js', '.json' ],
